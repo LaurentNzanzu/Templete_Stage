54dcdeacfec48d37861173a4df9f04b2afe60814
Happy Kitsa <happykitsa5@gmail.com>
2026-04-09 17:27:45 +0200

    upload js file

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 AOS.init({
   duration: 1000, // Durée de l'animation en millisecondes (1s)
-  once: true,     // L'animation ne se joue qu'une seule fois
-  offset: 120,    // Déclenche l'anim 120px avant que l'élément n'arrive
-  easing: 'ease-out-back' // Courbe d'accélération fluide
+  once: true,     
+  offset: 120,    
+  easing: 'ease-out-back' 
 });
 // On récupère tous les liens qui pointent vers le haut
 const backToHomeLinks = document.querySelectorAll('.service-link');
 
 backToHomeLinks.forEach(link => {
   link.addEventListener('click', (e) => {
-    e.preventDefault(); // On empêche le saut brutal du navigateur
+    e.preventDefault(); 
 
     // On cible la section de destination (ici #hero)
     const target = document.querySelector('#hero');
     
     // On déclenche le défilement fluide "fait main"
     target.scrollIntoView({
-      behavior: 'smooth', // Le mouvement est fluide
-      block: 'start'      // On s'arrête exactement au début de la section
+      behavior: 'smooth', 
+      block: 'start'      
     });
   });
 });
